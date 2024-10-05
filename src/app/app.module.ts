7ea3df0b5811fff32bfeabaddfe06901702bf454
= <rezabahmani085@gmail.com>
2024-10-05 17:46:12 +0330

    Move the app inside the folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './users/users.module';
+import { UserModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/users.entity';
-import { AuthModule } from './auth/auth.module';
-import { ProductModule } from './products/products.module';
-import { Product } from './products/entities/products.entity';
-import { BasketsModule } from './baskets/baskets.module';
-import { Basket } from './baskets/entities/baskets';
-import { BasketProduct } from './baskets/entities/basket-product';
-import { BlogModule } from './blog/blog.module';
-import { Post } from './blog/entities/posts';
+import { User } from '../users/entities/users.entity';
+import { AuthModule } from '../auth/auth.module';
+import { ProductModule } from '../products/products.module';
+import { Product } from '../products/entities/products.entity';
+import { BasketsModule } from '../baskets/baskets.module';
+import { Basket } from '../baskets/entities/baskets';
+import { BasketProduct } from '../baskets/entities/basket-product';
+import { BlogModule } from '../blog/blog.module';
+import { Post } from '../blog/entities/posts';
 
 @Module({
   imports: [
