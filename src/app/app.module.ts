a862359acf9d5cefc32836bebe91dc8461833a9c
= <rezabahmani085@gmail.com>
2024-10-12 16:07:06 +0330

    redis password env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { BullModule } from '@nestjs/bull';
       redis: {
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+        password:process.env.REDIS_PASSWORD || ''
       },
     }),
     TypeOrmModule.forFeature([App]),
